8bcbac17f3f0a6baf5a11d4c3c89e5bdd98140cb
JayanthVeerappa <132962391+JayanthVeerappa@users.noreply.github.com>
2024-06-11 13:05:34 -0700

    Update max.py

--- max.py
+++ max.py
@@ -1,1 +1,14 @@
+print ("max")
+a = int(input("Pick a number: "))
+b = int(input("Pick a number: "))
+c = int(input("Pick a number: "))
 
+if (a >= b) and (a >= c):
+    largest = a
+
+elif (b >= a) and (b >= c):
+    largest = b
+else:
+    largest = c
+
+print ("The max is ", largest)
