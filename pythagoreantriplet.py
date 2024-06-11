dacbf78fb789a28828da2972d0242beffd08157c
JayanthVeerappa <132962391+JayanthVeerappa@users.noreply.github.com>
2024-06-11 13:09:05 -0700

    Create pythagoreantriplet.py

---
+++ pythagoreantriplet.py
@@ -0,0 +1,9 @@
+print ("pythagorean triplet")
+a = int(input("Pick a number: "))
+b = int(input("Pick a number: "))
+c = int(input("Pick a number: "))
+
+if a**2+b**2==c**2:
+   print ("It is a pythagorean triplet")
+else:
+    print ("it is not a pythagorean triplet ")
