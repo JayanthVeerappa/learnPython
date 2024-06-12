2792b07ffa96c6b749bc1712a51fac652d3edcee
JayanthVeerappa <132962391+JayanthVeerappa@users.noreply.github.com>
2024-06-12 16:57:04 -0700

    Create n!.py

---
+++ n!.py
@@ -0,0 +1,11 @@
+def factorial(n: int) -> int:
+	ans_local = 1
+	for i in range(2, n + 1):
+		ans_local = ans_local * i
+	return ans_local
+
+
+print("Program to print n!")
+n = int(input("Type a number (n): "))
+ans = factorial(n)
+print(f"The factorial of {n} is {ans}")
