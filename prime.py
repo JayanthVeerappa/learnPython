85d8187be84df1d7034e17033d3e87f12f9e11ff
JayanthVeerappa <132962391+JayanthVeerappa@users.noreply.github.com>
2024-06-14 12:58:19 -0700

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,12 @@
+print("prime number checker")
+n = int(input("Type a number and i will check if it is a prime number: "))
+numofdivisors= []
+
+for i in range(1,n+1):
+    if n%i==0:
+        numofdivisors.append(i)
+
+if len(numofdivisors)>2:
+    print ("Not prime")
+else:
+    print ("prime")
