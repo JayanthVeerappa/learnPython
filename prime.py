2e1841ff04a92cccda013a09763a305b488031ba
JayanthVeerappa <132962391+JayanthVeerappa@users.noreply.github.com>
2024-06-16 12:46:11 -0700

    Update prime.py

--- prime.py
+++ prime.py
@@ -5,8 +5,9 @@ numofdivisors= []
 for i in range(1,n+1):
     if n%i==0:
         numofdivisors.append(i)
-
-if len(numofdivisors)>2:
-    print ("Not prime")
-else:
-    print ("prime")
+def primechecker():
+    if len(numofdivisors)>2:
+        print ("Not prime")
+    else:
+        print ("prime")
+primechecker()
